32f37062a8c6c38eb725f4399d6a6083da3f79a2
Mr.tao <staugur@saintic.com>
2019-09-26 14:13:27 +0800

    Slightly supplement the ping interface

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -4,24 +4,22 @@ module.exports = {
         //general
         name: 'tdi-node',
         script: 'server.js', //启动执行的初始脚本
-        instances: "max",
-        exec_mode: "cluster",
+        instances: 'max',
+        exec_mode: 'cluster',
 
         //advanced
-        watch: ['ecosystem.config.js', 'server.js'], //监听文件变化
-        ignore_watch: ['node_modules', ], //忽略监听的文件夹
+        watch: ['server.js'], //监听文件变化
+        ignore_watch: ['node_modules', 'logs'], //忽略监听的文件夹
         max_memory_restart: '1024M', //内存达到多少会自动restart
         env: {
-            "NODE_ENV": "production"
+            'NODE_ENV': 'production'
         },
-
-        //log file
-        log_date_format: 'YYYY-MM-DD HH:mm:ss Z', //日志格式
+        log_file: './logs/sys.log',
+        log_date_format: 'YYYY-MM-DD HH:mm',
+        merge_logs: true,
 
         //control
-        min_uptime: 3000,
         listen_timeout: 3000,
-        kill_timeout: 5000,
-        max_restarts: 5,
+        kill_timeout: 5000
     }]
 };
--- src/server.js
+++ src/server.js
@@ -3,18 +3,55 @@
  * Copyright 2019, MIT LICENSE.
  */
 
-const host = "127.0.0.1"
-const port = 3000
+//配置信息
+const HOST = "127.0.0.1"
+const PORT = 3000
+const VERSION = require('./package.json').version
+const STATUS = 'enabled'
+const ALARMEMAIL = ''
 
+//接口实现
 var express = require('express');
 var app = express();
+var {
+    totalmem,
+    freemem,
+    loadavg,
+} = require('os');
+var {
+    existsSync,
+    mkdirSync
+} = require('fs');
+var diskusage = require('diskusage-ng');
 
+const DOWNLOAD_DIR = __dirname + '/downloads';
+if (!existsSync(DOWNLOAD_DIR)) {
+    mkdirSync(DOWNLOAD_DIR);
+}
 app.get('/ping', function (req, res) {
-    res.json({
-        msg: 'Hello World!'
+    let resp = {
+        code: 0,
+        version: VERSION,
+        status: STATUS,
+        'memRate': parseFloat((100 * (totalmem() - freemem()) / totalmem()).toFixed(2)),
+        'loadFive': parseFloat(loadavg()[1].toFixed(2)),
+        'diskRate': 0,
+        'timestamp': Math.round(Date.now() / 1000),
+        'rqcount': 0,
+        'rqfailed': 0,
+        'email': ALARMEMAIL,
+        'lang': 'Node.js ' + process.versions.node
+    }
+    diskusage(DOWNLOAD_DIR, (err, usage) => {
+        resp["diskRate"] = parseFloat((usage.used / (usage.used + usage.available) * 100).toFixed(2));
+        res.json(resp);
     });
 });
 
-app.listen(port, host, function () {
-    console.log(`Server running at http://${host}:${port}`);
+app.use(function (req, res, next) {
+    res.status(404).send('Not found page');
+});
+
+app.listen(PORT, HOST, function () {
+    console.log(`Server running at http://${HOST}:${PORT}`);
 });
